52a82f1872827972d8e860cff91106785b429eac
EC2 Default User <ec2-user@ip-172-31-128-7.eu-west-1.compute.internal>
2022-01-25 14:34:53 +0000

    initial commit

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,8 +11,12 @@ export class MyPipelineStack extends cdk.Stack {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection('slungelongcobo/cdktest2', 'main', {connectionArn: 'arn:aws:codestar-connections:eu-west-1:065381300161:connection/615f1d1c-1af1-49ce-9bd7-81c62b51cec3'}),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        commands: ['npm install -g aws-cdk', 'npm run build', 'npx cdk synth']
       })
     });
+    
+      pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "065381300161", region: "eu-west-1" }
+    }));
   }
 }
